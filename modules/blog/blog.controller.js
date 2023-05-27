e5366152dde27f8f94d738457bb2ee28b5ef81c1
Arpan <shaharpan05@gmail.com>
2023-05-27 23:43:50 +0530

    revert metadata code

--- modules/blog/blog.controller.js
+++ modules/blog/blog.controller.js
@@ -73,9 +73,7 @@ const generateBlogPreview = async (req, res) => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>${blogData.title}</title>
       
-      <meta property="og:url" content="https://www.snssindia.in/${type}/${url}" />
       <meta property="og:title" content="${blogData.title}" />
-      <meta property="og:type" content="article" />
       <meta property="og:description" content="${blogData.description}" />
       <meta property="og:image" content="${bucketUrl}/${blogData.poster_image}" />
 
