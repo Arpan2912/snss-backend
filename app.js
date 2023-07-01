cf84d1c6c739b51f7ab7735e10f44ed5e7856062
Arpan <shaharpan05@gmail.com>
2023-07-01 17:32:53 +0530

    update file size problem

--- app.js
+++ app.js
@@ -23,14 +23,14 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: '10mb' }))
+app.use(bodyParser.json({ limit: '10mb' }));
 app.use(cors());
 
 // app.use('/', indexRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/news', blogRouter);
-app.use('/*',express.static(path.join(__dirname, 'public')));
+app.use('/*', express.static(path.join(__dirname, 'public')));
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
